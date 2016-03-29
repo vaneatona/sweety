c433b5e9513a6f9c6f4c2d14eae99e7d9f6a9cf0
Andrew <vaneatona@gmail.com>
2016-03-29 10:49:21 -0700

    Cleans up some code smell with commented out code and properly includes factory_girl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rspec'
-require 'support/auth_logic_helpers'
 require 'authlogic/test_case'
 include Authlogic::TestCase
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -24,7 +23,7 @@ include Authlogic::TestCase
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
